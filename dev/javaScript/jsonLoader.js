d17f5603a0ae1f711b89d9e49b3dd86703f7ddaf
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2022-04-11 19:13:26 +0900

    Delete dev directory

--- dev/javaScript/jsonLoader.js
+++
@@ -1,9 +0,0 @@
-﻿function loadJson(_path, _onCompleted) {
-	let req = new XMLHttpRequest();
-	req.open("get", _path, true);
-	req.send();
-	req.onload = function () {
-		let jsonText = JSON.parse(req.responseText);
-		_onCompleted(jsonText);
-	}
-}
