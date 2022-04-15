7940756ed91dda34e942023a2bdf2bbc48385b8f
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2022-04-15 14:45:13 +0900

    Add files via upload

---
+++ dev/javaScript/jsonLoader.js
@@ -0,0 +1,9 @@
+﻿function loadJson(_path, _onCompleted) {
+	let req = new XMLHttpRequest();
+	req.open("get", _path, true);
+	req.send();
+	req.onload = function () {
+		let jsonText = JSON.parse(req.responseText);
+		_onCompleted(jsonText);
+	}
+}
