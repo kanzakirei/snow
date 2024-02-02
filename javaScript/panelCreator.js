6a842fc0172c834c0888b8af9710fb2d5f0c7e9d
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2024-02-02 19:55:35 +0900

    Update panelCreator.js

--- javaScript/panelCreator.js
+++ javaScript/panelCreator.js
@@ -1,8 +1,8 @@
 function createList(_json) {
 	let target = document.getElementById("panelList");
 	let openDiv = document.createElement("div");
-	let closeDiv = document.createElement("div");
 	target.appendChild(openDiv);
+	let closeDiv = document.createElement("div");
 	target.appendChild(closeDiv);
 	let openDivs = {};
 	let closeDivs = {};
@@ -14,16 +14,18 @@ function createList(_json) {
 		let area = data.url.slice(-2);
 		if (openDate > currentDate || currentDate > closeDate) {
 			if(!closeDivs[area]) {
-			 closeDivs[area] = document.createElement("div");
-			 closeDiv.appendChild(closeDivs[area]);
-		 }
+		 		closeDivs[area] = document.createElement("div");
+					closeDivs[area].classList.add("line");
+					closeDiv.appendChild(closeDivs[area]);
+			}
 			createFrame(openDate, closeDate, closeDivs[area], `${data.name}\n(${data.open} ~ ${data.close})`, data.url);
 		}
 		else {
 			if(!openDivs[area]) {
-			 openDivs[area] = document.createElement("div");
-			 openDiv.appendChild(openDivs[area]);
-		 }
+		 		openDivs[area] = document.createElement("div");
+					openDivs[area].classList.add("line");
+					openDiv.appendChild(openDivs[area]);
+ 			}
 			createFrame(openDate, closeDate, openDivs[area], `${data.name}\n(${data.open} ~ ${data.close})`, data.url);
 		}
 	});
@@ -35,15 +37,16 @@ function createFrame(_openDate, _closeDate, _parent, _name, _url) {
 	div.classList.add("frame");
 	_parent.appendChild(div);
 
-	createName(_openDate, _closeDate, div, _name);
+	createName(_openDate, _closeDate, div, _name, _url);
 	if (_url) {
 		createIframeBlock(div, _url);
 	}
 }
 
-function createName(_openDate, _closeDate, _parent, _name) {
-	let p = document.createElement("p");
+function createName(_openDate, _closeDate, _parent, _name, _url) {
+	let p = document.createElement("a");
 	p.innerText = _name;
+	p.href = _url;
 	var currentDate = new Date();
 	if (_openDate > currentDate || currentDate > _closeDate) {
 		p.classList.add("color_red");
